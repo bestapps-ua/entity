a9738ce66631e472e278168c7f70416c4054abd7
Vitaliy Grinchishin <vitaliy.grinchishin@gmail.com>
2023-06-14 10:50:23 +0000

    1.0.3

--- dist/src/model/event/Events.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EVENT_ENTITY_UPDATED = exports.EVENT_ENTITY_CREATED = void 0;
-const EVENT_ENTITY_CREATED = 'entity::created';
-exports.EVENT_ENTITY_CREATED = EVENT_ENTITY_CREATED;
-const EVENT_ENTITY_UPDATED = 'entity::updated';
-exports.EVENT_ENTITY_UPDATED = EVENT_ENTITY_UPDATED;
-//# sourceMappingURL=Events.js.map
