d3db4023fa4ba61f3b7bdd5328fec694a66f352d
Vitaliy Grinchishin <vitaliy.grinchishin@gmail.com>
2024-12-18 09:46:07 +0000

    added push

---
+++ push.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
+npm run build
+git add dist/*
+git commit -m "$VERSION" package.json package-lock.json dist/ src/ push.sh;
+git push origin master;
+git tag $VERSION;
+git push origin $VERSION;
+npm publish --access=public
+
+
